894b40b1a927624803bbe4576fb4277777ce9c75
JC BUISSON <buisson@enseeiht.fr>
2024-03-09 14:11:25 +0100

    WIP

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="m-2">
+      <div class="w-full p-4">
+         <AnnotatedBlock :text="text" />
+      </div>
+      <p class="mt-5">{{ state }}</p>
+   </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import AnnotatedBlock from '/src/components/AnnotatedBlock.vue'
+
+const text = "Le processus d'inflammation aiguë est initié par les cellules immunitaires résidentes déjà présentes dans le tissu impliqué, principalement les macrophages résidents, les cellules dendritiques, les histiocytes, les cellules de Kupffer et les mastocytes."
+
+</script>
---
+++ src/components/AnnotatedBlock.vue
@@ -0,0 +1,64 @@
+<template>
+   <template v-for="part of parts">
+      <span :class="{ 'bg-yellow-200': part.highlighted }" @mouseup="onMouseUp(part)" v-html="part.text"></span>
+   </template>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useLocalStorage } from '@vueuse/core'
+
+
+const props = defineProps({
+   text: {
+      type: String,
+      required: true
+   },
+})
+
+const parts = useLocalStorage('annotated-parts', [
+   {
+      text: props.text,
+      start: 0,
+      end: props.text.length,
+      highlighted: false,
+   }
+])
+
+function onMouseUp(part) {
+   const selection = window.getSelection()
+   // selection must not be empty
+   if (!selection || selection.rangeCount === 0) return
+   const range = selection.getRangeAt(0)
+   if (range.startOffset === range.endOffset) return
+
+   console.log('part', part)
+   console.log('range', range)
+   const partIndex = parts.value.indexOf(part)
+   if (range.startOffset === part.start) {
+      // replace part by 2 sub-parts, the left one being highlighted
+   } else if (range.endOffset === part.end) {
+      // replace part by 2 sub-parts, the right one being highlighted
+   } else {
+      // replace part by 3 sub-parts, the middle one being highlighted
+      parts.value.splice(partIndex, 1, ...[
+      {
+            text: part.text.substring(0, range.startOffset),
+            start: part.start,
+            end: part.start + range.startOffset,
+         },
+         {
+            text: part.text.substring(range.startOffset, range.endOffset),
+            start: part.start + range.startOffset,
+            end: part.start + range.endOffset,
+            highlighted: true,
+         },
+         {
+            text: part.text.substring(range.endOffset, part.end),
+            start: part.start + range.endOffset,
+            end: part.end,
+         },
+      ])
+   }
+}
+</script>
